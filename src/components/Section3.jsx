ac330ded47d63acb17a0f0f9e8c04a922d8b3728
Amanda Zotelli <amzotelli@gmail.com>
2022-05-10 20:14:51 -0300

    :construction: (em progresso) adiciona a terceira seção

---
+++ src/components/Section3.jsx
@@ -0,0 +1,22 @@
+import Image from 'next/image';
+
+import Title from '../components/Title';
+import styles from '../styles/Container.module.css';
+
+export default function Section3() {
+  return (
+    <section className={ styles.container3 }>
+      <div className={ styles.background }>
+        <div className={ styles.aha2 }>
+          <Title title="THE EVENT REWARDS" />
+          <div className={ styles.paragraph }>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac ipsum, cursus habitasse eu a, ac aliquet habitant eu. Maecenas tincidunt duis scelerisque varius vel elementum.
+            </p>
+          </div>
+            <Image src="/images/section3/img-pubggraffitti.png" alt="background" width="300px" height="100%" />          
+        </div>
+      </div>
+    </section>
+  )
+}
