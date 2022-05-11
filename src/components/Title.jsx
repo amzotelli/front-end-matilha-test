bb0819386d5020e5245a7258a5ce7abceffb4d75
Amanda Zotelli <amzotelli@gmail.com>
2022-05-11 11:22:36 -0300

    :recycle: refactor: refatora o componente Title

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -3,10 +3,8 @@ import styles from '../styles/Title.module.css';
 export default function Title(props) {
   return (
     <section>
-      <div>
         <h5 className={ styles.subtitle }>VIKENDI WONDERLAND GIVEAWAY</h5>
         <h3 className={ styles.title }>{ props.title }</h3>
-      </div>
     </section>
   )
 }
