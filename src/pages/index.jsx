4cc60bb7a69b21c2649d7f0c929a651c7965f80b
Amanda Zotelli <amzotelli@gmail.com>
2022-05-12 00:37:33 -0300

    :sparkles: feat: implementa componentes em index

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,21 +3,22 @@ import Head from 'next/head'
 import NavBar from '../components/NavBar';
 import Header from '../components/Header';
 import Main from '../components/Main';
-import AwardSection from '../components/AwardSection';
+import AwardsSection from '../components/AwardsSection';
+import StepsSection from '../components/StepsSection';
+import Footer from '../components/Footer';
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Head>
         <title>teste matilha</title>
       </Head>
       <NavBar />
       <Header />
       <Main />
-      <AwardSection />
-      <footer>
-        </footer>
-        
-    </div>
+      <AwardsSection />
+      <StepsSection />
+      <Footer />        
+    </>
   )
 }
