8824eed396809879746a55192f84ae78e7358f60
Amanda Zotelli <amzotelli@gmail.com>
2022-05-12 09:31:52 -0300

    :recycle: refactor: move imagem do arquivo Footer para o CSS

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,9 @@
-import Image from 'next/image';
-
 import styles from '../styles/Footer.module.css';
 
 export default function Footer() {
   return (
     <footer>
-      <div className={styles.divider}>
-        <Image
-          src="/images/footer/divider-bottom.png"
-          alt="logo"
-          width="1440px"
-          height="32px" 
-          objectFit="contain"
-        />
-      </div>
+      <div className={styles.divider} />
       <div className={styles.footerContainer} />
     </footer>
   );
