eef25f9f8a75503b4ae639a14b191fe23fb2563c
Amanda Zotelli <amzotelli@gmail.com>
2022-05-12 00:38:09 -0300

    :recycle: refactor: corrige erros de lint e style lint em main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,11 +3,11 @@ import Image from 'next/image';
 import Title from './subcomponents/Title';
 import styles from '../styles/Main.module.css';
 
-export default function Section2() {
+export default function Main() {
   return (
     <section className={ styles.container }>
       <div className={ styles.secondContainer }>
-      <div className={ styles.text }>
+        <div className={ styles.text }>
           <Title title="ABOUT THE EVENT" />
           <Image
             src="/images/main/grad-line.png"
@@ -16,7 +16,7 @@ export default function Section2() {
             alt="color"
           />
         </div>
-        <div className={ styles.secondDetail }>
+        <div className={ styles.sideImage }>
           <Image
             src="/images/main/img-characters.png"
             alt="Characters"
@@ -25,22 +25,25 @@ export default function Section2() {
             objectFit="cover"
           />
         </div>
-      <div className={ styles.secondMain }>
-        <button type="button" className={ styles.secondLogin }>
+        <div className={ styles.paragraph }>
+          <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Ac ipsum, cursus habitasse eu a, ac aliquet habitant eu.
+          Maecenas tincidunt duis scelerisque varius vel elementum nascetur aliquet.
+          Nibh risus leo non onvallis dolor quis gravida pretium, consequat.
+          Sit sed et erat dui. Feugiat tellus ipsum amet nisl elit suspendisse cras mattis.
+          </p>
+          <p>
+          Pellentesque nisl bibendum feugiat adipiscing quisque velit mauris.
+          Amet pellentesque amet eleifend pulvinar tincidunt.Convallis neque consectetur massa ipsum. 
+          </p>
+        </div>
+        <div className={ styles.textLink }>
+          <h5>SEE THE OFFICIAL RULES</h5>
+        </div>
+        <button type="button" className={ styles.loginButton }>
           LOGIN OR CREATE ACCOUNT
         </button>
-        <div className={ styles.paragraph }>
-        <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ac ipsum, cursus habitasse eu a, ac aliquet habitant eu. Maecenas tincidunt duis scelerisque varius vel elementum nascetur aliquet. Nibh risus leo non onvallis dolor quis gravida pretium, consequat. Sit sed et erat dui. Feugiat tellus ipsum amet nisl elit suspendisse cras mattis.
-            </p>
-            <p>
-            Pellentesque nisl bibendum feugiat adipiscing quisque velit mauris.Amet pellentesque amet eleifend pulvinar tincidunt.Convallis neque consectetur massa ipsum. 
-            </p>
-          </div>
-          <div className={ styles.link }>
-            <h5>SEE THE OFFICIAL RULES</h5>
-          </div>
-        </div>
       </div>
     </section>
   )
