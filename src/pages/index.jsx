e49ec22a0446fbce743cbbf97b74d213e72e6d1e
Amanda Zotelli <amzotelli@gmail.com>
2022-05-12 20:54:59 -0300

    :pencil: edita título do index

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>teste matilha</title>
+        <title>Teste Amanda&&Matilha</title>
       </Head>
       <NavBar />
       <Header />
